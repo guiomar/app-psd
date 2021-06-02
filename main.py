8981637ddbba4acc330cff627104ecfe5089a334
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-06-02 17:02:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ fname = config['fif']
 
 fmin = config['fmin']
 # fmax=config['fmax'] if config['fmax'] else np.inf
-fmax=config['fmax'] 
+fmax=float(config['fmax'])
 average = config['average']
 
 # Advanced parameters
