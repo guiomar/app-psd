b79b2ef34309e8f573d42efb81570118511a82fc
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-06-02 15:48:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,17 +28,27 @@ fname = config['fif']
 
 fmin = config['fmin']
 fmax = config['fmax']
-#tmin = config['tmin']
-#tmax = config['tmax']
-#n_fft = config['n_fft']
-#n_overlap = config['n_overlap']
+average = config['average']
+
+# Advanced parameters
+picks = config['picks']
+tmin = config['tmin']
+tmax = config['tmax']
+n_fft = config['n_fft']
+n_overlap = config['n_overlap']
+n_per_seg = config['n_per_seg']
+window = config['window']
+proj = config['proj']
+reject_by_annotation = config['reject_by_annotation']
+#n_jobs = config['n_jobs']
+#verbose = config['verbose']
 
 
 raw = mne.io.read_raw_fif(fname)
 
-psds_welch, freqs = mne.time_frequency.psd_welch(raw, fmin=fmin, fmax=fmax, tmin=None, tmax=None, 
-                             n_fft=256, n_overlap=0, n_per_seg=None, picks=None, proj=False, n_jobs=1, 
-                             reject_by_annotation=True, average='mean', window='hamming', verbose=None)
+psds_welch, freqs = mne.time_frequency.psd_welch(raw, fmin=fmin, fmax=fmax, tmin=tmin, tmax=tmax, 
+                             n_fft=n_fft, n_overlap=n_overlap, n_per_seg=n_per_seg, window=window, picks=picks, proj=proj,
+                             reject_by_annotation=reject_by_annotation, average=average, n_jobs=1, verbose=None)
 
 # Convert power to dB scale.
 psds_welch = 10 * np.log10(psds_welch)
