baa4f24e67d7af363ec29b498c3eb18806398e04
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-06-07 20:36:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,10 +44,11 @@ reject_by_annotation = config['reject_by_annotation']
 #n_jobs = config['n_jobs']
 #verbose = config['verbose']
 
+picks= ['MEG 0112', 'MEG 0113'] 
+
 print(tmin)
 print(picks)
 
-picks= ['MEG 0112', 'MEG 0113'] 
 
 raw = mne.io.read_raw_fif(fname)
 
