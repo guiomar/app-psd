30e8da5ead38de52281d9f57d6887cf51e5ae4f0
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-10-22 19:41:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ if picks==None:
         # Save to CSV file (could be also TSV)
         df_psd = pd.DataFrame(psd_welch_eeg, index=ch_eeg, columns=freqs_eeg)
         df_psd.index.name='channels'
-        df_psd.to_csv(os.path.join('out_psd_eeg','psd_eeg.csv')) #, sep = '\t', index=False)
+        df_psd.to_csv(os.path.join('out_psd_eeg','psd.csv')) #, sep = '\t', index=False)
 
         # Figure
         axs[aa].plot(freqs_eeg, psd_welch_eeg.transpose(), zorder=1) 
@@ -131,7 +131,7 @@ if picks==None:
         # Save to CSV file (could be also TSV)
         df_psd = pd.DataFrame(psd_welch_grad, index=ch_grad, columns=freqs_grad)
         df_psd.index.name='channels'
-        df_psd.to_csv(os.path.join('out_psd_grad','psd_grad.csv')) #, sep = '\t', index=False)
+        df_psd.to_csv(os.path.join('out_psd_grad','psd.csv')) #, sep = '\t', index=False)
 
         # Figure
         axs[aa].plot(freqs_grad, psd_welch_grad.transpose(), zorder=1) 
@@ -160,7 +160,7 @@ if picks==None:
         # Save to CSV file (could be also TSV)
         df_psd = pd.DataFrame(psd_welch_mag, index=ch_mag, columns=freqs_mag)
         df_psd.index.name='channels'
-        df_psd.to_csv(os.path.join('out_psd_mag','psd_mag.csv')) #, sep = '\t', index=False)
+        df_psd.to_csv(os.path.join('out_psd_mag','psd.csv')) #, sep = '\t', index=False)
 
         # Figure
         axs[aa].plot(freqs_mag, psd_welch_mag.transpose(), zorder=1) 
